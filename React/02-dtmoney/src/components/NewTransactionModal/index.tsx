1a135ef6a5c10803334fd9b867e64a56ecb02beb
David <davidwdiniz@gmail.com>
2021-09-30 19:34:37 -0300

    Button operation

--- React/02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ React/02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,14 +1,16 @@
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import {useState} from "react";
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+    const [type, setType] = useState("deposit");
     return (
         <Modal
             isOpen={isOpen}
@@ -26,14 +28,22 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 <input placeholder="Título" />
                 <input type="number" placeholder="Valor"/>
                 <TransactionTypeContainer>
-                    <button type="button">
+                    <RadioBox
+                        type="button"
+                        onClick={() => setType("deposit")}
+                        isActive={type === "deposit"}
+                    >
                         <img src={incomeImg} alt="Entrada"/>
                         <span>Entrada</span>
-                    </button>
-                    <button type="button">
+                    </RadioBox>
+                    <RadioBox
+                        type="button"
+                        onClick={() => setType("withdraw")}
+                        isActive={type === "withdraw"}
+                    >
                         <img src={outcomeImg} alt="Saída"/>
                         <span>Saída</span>
-                    </button>
+                    </RadioBox>
                 </TransactionTypeContainer>
                 <input placeholder="Categoria"/>
                 <button type="submit">
