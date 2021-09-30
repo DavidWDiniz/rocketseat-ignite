259c52b87c2a1dbfce774d61058a5c0bfa9b0ceb
David <davidwdiniz@gmail.com>
2021-09-30 20:48:53 -0300

    Listing transactions and seed

--- React/02-dtmoney/src/components/TransactionsTable/index.tsx
+++ React/02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,11 +1,21 @@
 import { Container } from "./styles";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {api} from "../../services/api";
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionsTable() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
     useEffect(() => {
         api.get("transactions")
-            .then(response => console.log(response.data))
+            .then(response => setTransactions(response.data.transactions))
     }, [])
     return (
         <Container>
@@ -19,18 +29,16 @@ export function TransactionsTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">R$ 1.000,00</td>
-                        <td>Cada</td>
-                        <td>20/05/2021</td>
-                    </tr>
+                {transactions.map(transaction => {
+                    return(
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>{transaction.amount}</td>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.createdAt}</td>
+                        </tr>
+                    )
+                })}
                 </tbody>
             </table>
         </Container>
