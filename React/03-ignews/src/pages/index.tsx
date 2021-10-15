87a75a05f92038921ef2a0f23b2fea21313f8e8f
David <davidwdiniz@gmail.com>
2021-10-15 10:05:09 -0300

    Home page

--- React/03-ignews/src/pages/index.tsx
+++ React/03-ignews/src/pages/index.tsx
@@ -1,10 +1,23 @@
+import Head from "next/head";
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
-      <title>Início | ig.news</title>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   );
 }
