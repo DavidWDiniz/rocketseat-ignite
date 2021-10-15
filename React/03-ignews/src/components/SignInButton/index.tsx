ce465299ea49e8dd1e3ad9e2c42eec6551aa4ef6
David <davidwdiniz@gmail.com>
2021-10-15 14:48:42 -0300

    Authentication with next-auth

--- React/03-ignews/src/components/SignInButton/index.tsx
+++ React/03-ignews/src/components/SignInButton/index.tsx
@@ -1,22 +1,26 @@
 import {FaGithub} from "react-icons/fa";
 import styles from "./styles.module.scss";
 import {FiX} from "react-icons/fi";
+import {signIn, signOut, useSession} from "next-auth/client";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const [session] = useSession();
+
+  return session ? (
       <button
         type="button"
         className={styles.signInButton}
+        onClick={() => signOut()}
       >
         <FaGithub color="#04d361"/>
-        David William
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
       </button>
     ) : (
       <button
         type="button"
         className={styles.signInButton}
+        onClick={() => signIn("github")}
       >
         <FaGithub color="#eba417"/>
         Sign in with Github
