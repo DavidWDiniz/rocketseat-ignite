9347837c51ff76bd5e239f576c8ef14e1b344578
David <davidwdiniz@gmail.com>
2021-10-18 11:36:05 -0300

    Working with Prismic CMS to add post pages to project

---
+++ React/03-ignews/src/components/ActiveLink/index.tsx
@@ -0,0 +1,18 @@
+import Link, {LinkProps} from "next/link";
+import {cloneElement, ReactElement} from "react";
+import {useRouter} from "next/router";
+
+interface ActiveLinkProps extends LinkProps{
+  children: ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink({activeClassName, children, ...rest}: ActiveLinkProps) {
+  const {asPath} = useRouter();
+  const className = asPath === rest.href ? activeClassName : "";
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {className})}
+    </Link>
+  )
+}
