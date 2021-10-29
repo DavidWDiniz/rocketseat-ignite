f7624d687a0eb922b96214ee987cf53520759c00
David <davidwdiniz@gmail.com>
2021-10-29 15:21:57 -0300

    Pagination configuration logic

--- React/04-dashgo/src/pages/users/index.tsx
+++ React/04-dashgo/src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Link,
   Spinner,
   Table,
   Tbody,
@@ -15,40 +16,33 @@ import {
   Tr,
   useBreakpointValue
 } from "@chakra-ui/react";
-import Link from "next/link";
+import NextLink from "next/link";
 import {Header} from "../../components/Header";
 import {Sidebar} from "../../components/Sidebar";
 import {RiAddLine} from "react-icons/ri";
 import {Pagination} from "../../components/Pagination";
-import {useQuery} from "react-query";
+import {useUsers} from "../../services/hooks/useUsers";
+import {useState} from "react";
+import {queryClient} from "../../services/queryClient";
+import {api} from "../../services/api";
 
 export default function UserList() {
-  const {data, isLoading, error} = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users")
-    const data = await response.json()
-    return data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString(
-          "pt-BR",
-          {
-            day: "2-digit",
-            month: "long",
-            year: "numeric"
-          }
-        )
-      }
-    })
-  }, {
-    staleTime: 1000 * 5 // 5 seconds
-  })
+  const [page, setPage] = useState(1)
+  const {data, isLoading, isFetching, error} = useUsers(page)
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
+
+  async function handlePrefetchUser(userId: number) {
+    await queryClient.prefetchQuery(["user", userId], async () => {
+      const response = await api.get(`users/${userId}`)
+      return response.data
+    }, {
+      staleTime: 1000 * 60 * 10 // 10 minutes
+    })
+  }
 
   return (
     <Box>
@@ -77,8 +71,9 @@ export default function UserList() {
               fontWeight="normal"
             >
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
             </Heading>
-            <Link
+            <NextLink
               href="/users/create"
               passHref
             >
@@ -91,7 +86,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
           {isLoading ? (
             <Flex
@@ -132,7 +127,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map(user => {
+                  {data.users.map(user => {
                     return (
                       <Tr key={user.id}>
                         <Td
@@ -144,11 +139,16 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <Text
-                              fontWeight="bold"
+                            <Link
+                              color="purple.400"
+                              onMouseEnter={() => handlePrefetchUser(user.id)}
                             >
-                              {user.name}
-                            </Text>
+                              <Text
+                                fontWeight="bold"
+                              >
+                                {user.name}
+                              </Text>
+                            </Link>
                             <Text
                               fontSize="sm"
                               color="gray.300"
@@ -159,7 +159,7 @@ export default function UserList() {
                         </Td>
                         {isWideVersion && (
                           <Td>
-                            {user.createdAt}
+                            {user.created_at}
                           </Td>
                         )}
                       </Tr>
@@ -167,7 +167,11 @@ export default function UserList() {
                   })}
                 </Tbody>
               </Table>
-              <Pagination/>
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
             </>
           )}
         </Box>
