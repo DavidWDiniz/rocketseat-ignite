6dfcbfa2cfa1132a4b1f108978752e7cca599bf5
David <davidwdiniz@gmail.com>
2021-11-01 11:59:36 -0300

    Authentication with JWT

---
+++ React/05-auth/contexts/AuthContext.tsx
@@ -0,0 +1,82 @@
+import {createContext, ReactNode, useEffect, useState} from "react";
+import {api} from "../services/api";
+import Router from "next/router";
+import {destroyCookie, parseCookies, setCookie} from "nookies";
+
+type User = {
+  permissions: string[]
+  roles: string[]
+  email: string
+}
+
+type SignInCredentials = {
+  email: string
+  password: string
+}
+
+type AuthContextData = {
+  user: User
+  signIn(credentials: SignInCredentials): Promise<void>
+  isAuthenticated: boolean
+}
+
+interface AuthProviderProps {
+  children: ReactNode
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function signOut() {
+  destroyCookie(undefined, "auth.token")
+  destroyCookie(undefined, "auth.refreshToken")
+  Router.push("/")
+}
+
+export function AuthProvider({children}: AuthProviderProps) {
+  const [user, setUser] = useState<User>({} as User)
+  const isAuthenticated = !!user
+
+  useEffect(() => {
+    const {"auth.token": token} = parseCookies()
+    if (token) {
+      api.get("/me").then(response => {
+        const {email, permissions, roles} = response.data
+        setUser({email, permissions, roles})
+      }).catch(() => {
+        signOut()
+      })
+    }
+  }, [])
+
+  async function signIn({email, password}: SignInCredentials) {
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password
+      })
+
+      const {token, refreshToken, permissions, roles} = response.data
+
+      setCookie(undefined, "auth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/"
+      })
+      setCookie(undefined, "auth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/"
+      })
+
+      setUser({email, permissions, roles})
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+      await Router.push("/dashboard")
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{signIn, isAuthenticated, user}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
---
+++ React/05-auth/services/api.ts
@@ -0,0 +1,72 @@
+import axios, {AxiosError} from "axios";
+import {parseCookies, setCookie} from "nookies";
+import {signOut} from "../contexts/AuthContext";
+
+let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestsQueue: { onSuccess: (token: string) => void; onFailure: (err: AxiosError<any, any>) => void; }[] = []
+
+export const api = axios.create({
+  baseURL: "http://localhost:3333",
+  headers: {
+    Authorization: `Bearer ${cookies["auth.token"]}`
+  }
+})
+
+api.interceptors.response.use(response => {
+  return response
+}, (error: AxiosError) => {
+  if (error.response?.status === 401) {
+    if (error.response.data?.code === "token.expired") {
+      cookies = parseCookies()
+      const {"auth.refreshToken": refreshToken} = cookies
+      const originalConfig = error.config
+
+      if (!isRefreshing) {
+        isRefreshing = true
+        api.post("/refresh", {
+          refreshToken,
+        }).then(response => {
+          const {token} = response.data
+
+          setCookie(undefined, "auth.token", token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: "/"
+          })
+
+          setCookie(undefined, "auth.refreshToken", response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: "/"
+          })
+
+          api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+          failedRequestsQueue.forEach(request => request.onSuccess(token))
+          failedRequestsQueue = []
+        }).catch(err => {
+          failedRequestsQueue.forEach(request => request.onFailure(err))
+          failedRequestsQueue = []
+        }).finally(() => {
+          isRefreshing = false
+        })
+      }
+
+      return new Promise((resolve, reject) => {
+        failedRequestsQueue.push({
+          onSuccess: (token: string) => {
+            if (originalConfig.headers) {
+              originalConfig.headers["Authorization"] = `Bearer ${token}`
+              resolve(api(originalConfig))
+            }
+          },
+          onFailure: (err: AxiosError) => {
+            reject(err)
+          }
+        })
+      })
+    } else {
+      signOut()
+    }
+
+    return Promise.reject(error)
+  }
+})
