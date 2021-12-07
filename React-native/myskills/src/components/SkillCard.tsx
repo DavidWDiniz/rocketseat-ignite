243d3d2f0a5cb1e0e2813fbe0d9ecbe89b22b8a7
David <davidwdiniz@gmail.com>
2021-12-07 12:37:32 -0300

    Add typescript

---
+++ React-native/myskills/src/components/SkillCard.tsx
@@ -0,0 +1,34 @@
+import {StyleSheet, Text, TouchableOpacity, TouchableOpacityProps} from "react-native";
+import React from "react";
+
+interface SkillCardProps extends TouchableOpacityProps {
+  skill: string;
+}
+
+export function SkillCard({skill, ...rest}: SkillCardProps) {
+  return (
+    <TouchableOpacity
+      style={styles.buttonSkill}
+      {...rest}
+    >
+      <Text style={styles.textSkill}>
+        {skill}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttonSkill: {
+    backgroundColor: '#1f1e25',
+    padding: 25,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  textSkill: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  }
+})
