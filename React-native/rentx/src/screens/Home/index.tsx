3820e96649d291dbf7dc8700792af6134f3ec2c9
David <davidwdiniz@gmail.com>
2022-01-19 10:23:27 -0300

    Home header component

--- React-native/rentx/src/screens/Home/index.tsx
+++ React-native/rentx/src/screens/Home/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import {StatusBar} from "react-native";
+import Logo from "../../assets/logo.svg";
+import {RFValue} from "react-native-responsive-fontsize";
 
 export function Home() {
   return (
     <Container>
-      <Title>HOME</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 10 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
