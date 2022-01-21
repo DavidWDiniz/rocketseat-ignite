40e8e33a30d419acbcce49786c39a9d905b11a8d
David <davidwdiniz@gmail.com>
2022-01-21 10:30:03 -0300

    Accessories component

--- React-native/rentx/src/screens/CarDetails/index.tsx
+++ React-native/rentx/src/screens/CarDetails/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Container, Header, CarImages, Content, Details, Description, Branding, Name, Rent, Period, Price, About } from "./styles";
+import { Container, Header, CarImages, Content, Details, Description, Branding, Name, Rent, Period, Price, Accessories, About } from "./styles";
 import {BackButton} from "../../components/BackButton";
 import {ImageSlider} from "../../components/ImageSlider";
+import {Accessory} from "../../components/Accessory";
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
 
 export function CarDetails() {
   return (
@@ -23,6 +30,15 @@ export function CarDetails() {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 Hp" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
 
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza
