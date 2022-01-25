edb90221fdac018b43a9d36f613e6e5fce3e0155
David <davidwdiniz@gmail.com>
2022-01-25 11:56:47 -0300

    Scheduling details interface

--- React-native/rentx/App.tsx
+++ React-native/rentx/App.tsx
@@ -5,7 +5,7 @@ import {Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold} from "@expo-
 import AppLoading from "expo-app-loading";
 import {ThemeProvider} from "styled-components/native";
 import theme from "./src/styles/theme";
-import {Scheduling} from "./src/screens/Scheduling";
+import {SchedulingDetails} from "./src/screens/SchedulingDetails";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +22,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling />
+      <SchedulingDetails />
     </ThemeProvider>
   );
 }
---
+++ React-native/rentx/src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Branding,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  RentalPeriod,
+  CalendarIcon,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal,
+  Footer
+} from "./styles";
+import {BackButton} from "../../components/BackButton";
+import {ImageSlider} from "../../components/ImageSlider";
+import {Accessory} from "../../components/Accessory";
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+import {Button} from "../../components/Button";
+import {Feather} from "@expo/vector-icons";
+import {RFValue} from "react-native-responsive-fontsize";
+import {useTheme} from "styled-components";
+
+export function SchedulingDetails() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {
+        }}/>
+      </Header>
+      <CarImages>
+        <ImageSlider imageUrl={["https://pngimg.com/uploads/audi/audi_PNG99491.png"]}/>
+      </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Branding>lamborghini</Branding>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg}/>
+          <Accessory name="3.2s" icon={AccelerationSvg}/>
+          <Accessory name="800 Hp" icon={ForceSvg}/>
+          <Accessory name="Gasolina" icon={GasolineSvg}/>
+          <Accessory name="Auto" icon={ExchangeSvg}/>
+          <Accessory name="2 pessoas" icon={PeopleSvg}/>
+        </Accessories>
+        <RentalPeriod>
+          <CalendarIcon>
+            <Feather
+              name="calendar"
+              size={RFValue(24)}
+              color={theme.colors.shape}
+            />
+          </CalendarIcon>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue>25/01/2022</DateValue>
+          </DateInfo>
+          <Feather
+            name="chevron-right"
+            size={RFValue(10)}
+            color={theme.colors.text}
+          />
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue>25/01/2022</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+
+        <RentalPrice>
+          <RentalPriceLabel>TOTAL</RentalPriceLabel>
+          <RentalPriceDetails>
+            <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$ 2900</RentalPriceTotal>
+          </RentalPriceDetails>
+        </RentalPrice>
+      </Content>
+
+      <Footer>
+        <Button title="Alugar agora" color={theme.colors.success}/>
+      </Footer>
+    </Container>
+  )
+}
