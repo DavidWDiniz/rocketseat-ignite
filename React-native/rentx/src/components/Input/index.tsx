966623512993cffca41d28b983bbfdb87e906041
David <davidwdiniz@gmail.com>
2022-02-08 11:03:45 -0300

    E-mail input

--- React-native/rentx/src/components/Input/index.tsx
+++ React-native/rentx/src/components/Input/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container , IconContainer, InputText} from "./styles";
 import {Feather} from "@expo/vector-icons";
 import {useTheme} from "styled-components";
 import { TextInputProps } from "react-native";
@@ -8,15 +8,18 @@ interface InputProps extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>["name"];
 }
 
-export function Input({iconName}: InputProps) {
+export function Input({iconName, ...rest}: InputProps) {
   const theme = useTheme();
   return (
     <Container>
-      <Feather
-        name={iconName}
-        size={24}
-        color={theme.colors.text_detail}
-      />
+      <IconContainer>
+        <Feather
+          name={iconName}
+          size={24}
+          color={theme.colors.text_detail}
+        />
+      </IconContainer>
+      <InputText {...rest} />
     </Container>
   )
 }
--- React-native/rentx/src/components/Input/styles.ts
+++ React-native/rentx/src/components/Input/styles.ts
@@ -1,5 +1,24 @@
 import styled from "styled-components/native";
+import {RFValue} from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
+  flex-direction: row;
+`;
 
+export const IconContainer = styled.View`
+  width: 55px;
+  height: 55px;
+  justify-content: center;
+  align-items: center;
+  margin-right: 2px;
+  background-color: ${({theme}) => theme.colors.background_secondary};
+`;
+
+export const InputText = styled.TextInput`
+  flex: 1;
+  background-color: ${({theme}) => theme.colors.background_secondary};
+  font-size: ${RFValue(15)}px;
+  font-family: ${({theme}) => theme.fonts.primary_400};
+  color: ${({theme}) => theme.colors.text};
+  padding: 0 23px;
 `;
