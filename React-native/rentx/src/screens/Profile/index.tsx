fcb2ea045150d4cd5f85ef3bb35c34474df68357
David <davidwdiniz@gmail.com>
2022-02-18 09:49:52 -0300

    Registration sessions

--- React-native/rentx/src/screens/Profile/index.tsx
+++ React-native/rentx/src/screens/Profile/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import {Container, Header, HeaderTop, HeaderTitle, LogoutButton, PhotoContainer, Photo, PhotoButton} from "./styles"
+import React, {useState} from "react";
+import {Container, Header, HeaderTop, HeaderTitle, LogoutButton, PhotoContainer, Photo, PhotoButton, Content, Options, Option, OptionTitle} from "./styles"
 import {BackButton} from "../../components/BackButton";
 import {useTheme} from "styled-components";
 import {useNavigation} from "@react-navigation/native";
 import {Feather} from "@expo/vector-icons";
 
 export function Profile() {
+  const [option, setOption] = useState<"dataEdit" | "passwordEdit">("passwordEdit");
   const theme = useTheme();
   const navigation = useNavigation();
 
@@ -15,6 +16,10 @@ export function Profile() {
 
   function handleSignOut() {
 
+  }
+
+  function handleOptionChange(optionSelected: "dataEdit" | "passwordEdit") {
+    setOption(optionSelected);
   }
 
   return (
@@ -45,6 +50,22 @@ export function Profile() {
           </PhotoButton>
         </PhotoContainer>
       </Header>
+      <Content>
+        <Options>
+          <Option
+            active={option === "dataEdit"}
+            onPress={() => handleOptionChange("dataEdit")}
+          >
+            <OptionTitle active={option === "dataEdit"}>Dados</OptionTitle>
+          </Option>
+          <Option
+            active={option === "passwordEdit"}
+            onPress={() => handleOptionChange("passwordEdit")}
+          >
+            <OptionTitle active={option === "passwordEdit"}>Trocar senha</OptionTitle>
+          </Option>
+        </Options>
+      </Content>
     </Container>
   );
 }
