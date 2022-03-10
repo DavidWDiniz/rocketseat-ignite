1eaa4a404a6af82190ba8461f5141a359f9d9012
David <davidwdiniz@gmail.com>
2022-03-10 09:39:28 -0300

    Observing logged user and logout

--- React-native/myshopping/src/components/Header/index.tsx
+++ React-native/myshopping/src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ButtonIcon } from '../ButtonIcon';
 
 import { Container, Title } from './styles';
+import auth from "@react-native-firebase/auth";
 
 type Props = {
   title: string;
@@ -9,6 +10,10 @@ type Props = {
 }
 
 export function Header({ title, showLogoutButton = false }: Props) {
+  async function handleLogout() {
+    await auth().signOut();
+  }
+
   return (
     <Container showLogoutButton={showLogoutButton}>
       <Title>
@@ -21,7 +26,7 @@ export function Header({ title, showLogoutButton = false }: Props) {
           icon="logout"
           color="alert"
           style={{ marginTop: 20 }}
-          onPress={() => {}}
+          onPress={handleLogout}
         />
       }
     </Container>
--- React-native/myshopping/src/routes/index.tsx
+++ React-native/myshopping/src/routes/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 import {SignIn} from "../screens/SignIn";
+import auth from "@react-native-firebase/auth";
+import {AppRoutes} from "./app.routes";
+
+interface User {
+  uid: string;
+}
 
 export function Routes() {
+  const [user, setUser] = useState<User | null>(null)
+  useEffect(() => {
+    return auth().onAuthStateChanged(userInfo => {
+      setUser(userInfo);
+    });
+  }, []);
   return (
     <NavigationContainer>
-      <SignIn />
+      {user ? <AppRoutes/> : <SignIn/>}
     </NavigationContainer>
   )
 }
