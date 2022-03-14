f2a1ab8aa97efb101c31e434489ef8dfc3fe5e39
David <davidwdiniz@gmail.com>
2022-03-14 10:38:44 -0300

    Finishing SignIn screen

--- React-native/gopizza/src/screens/SignIn/index.tsx
+++ React-native/gopizza/src/screens/SignIn/index.tsx
@@ -1,28 +1,44 @@
 import React from "react";
-import { Container } from "./styles";
+import {Brand, Container, Content, ForgotPasswordButton, ForgotPasswordButtonLabel, Title} from "./styles";
 import {Input} from "../../components/Input";
 import {Button} from "../../components/Button";
+import {KeyboardAvoidingView, Platform} from "react-native";
+
+import brandImg from "../../assets/brand.png";
 
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : undefined}>
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
 
-      <Input
-        placeholder="Senha"
-        type="secondary"
-        secureTextEntry
-      />
+          <Input
+            placeholder="Senha"
+            type="secondary"
+            secureTextEntry
+          />
 
-      <Button
-        title="Entrar"
-        type="secondary"
-      />
+          <ForgotPasswordButton>
+            <ForgotPasswordButtonLabel>
+              Esqueci minha senha
+            </ForgotPasswordButtonLabel>
+          </ForgotPasswordButton>
+
+          <Button
+            title="Entrar"
+            type="secondary"
+          />
+
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
