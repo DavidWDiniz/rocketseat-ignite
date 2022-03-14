90d217a8ae1d6f1f9a0278083f250f9ecec8a322
David <davidwdiniz@gmail.com>
2022-03-14 11:44:59 -0300

    Firebase authentication implementation

--- React-native/gopizza/src/hooks/auth.tsx
+++ React-native/gopizza/src/hooks/auth.tsx
@@ -1,6 +1,19 @@
-import React, {createContext, ReactNode, useContext} from "react";
+import React, {createContext, ReactNode, useContext, useState} from "react";
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
+import {Alert} from "react-native";
 
-type AuthContextData = {}
+type User = {
+  id: string;
+  name: string;
+  isAdmin: boolean;
+}
+
+type AuthContextData = {
+  signIn: (email: string, password: string) => Promise<void>;
+  isLogging: boolean;
+  user: User | null;
+}
 
 type AuthProviderProps = {
   children: ReactNode;
@@ -9,8 +22,51 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({children}: AuthProviderProps) {
+  const [isLogging, setIsLogging] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+
+  async function signIn(email: string, password: string) {
+    if(!email || !password) {
+      return Alert.alert("Login", "Informe o e-mail e a senha.");
+    }
+    setIsLogging(true);
+    auth().signInWithEmailAndPassword(email, password)
+      .then(account => {
+        firestore()
+          .collection("users")
+          .doc(account.user.uid)
+          .get()
+          .then(profile => {
+            const {name, isAdmin} = profile.data() as User;
+            if (profile.exists) {
+              const userData = {
+                id: account.user.uid,
+                name,
+                isAdmin
+              }
+              setUser(userData);
+            }
+          })
+          .catch(() => Alert.alert("Login", "Não foi possível buscar os dados do perfil do usuário"));
+      })
+      .catch(error => {
+        const {code} = error;
+        if(code === "auth/user-not-found" || code === "auth/wrong-password") {
+          return Alert.alert("Login", "E-mail e/ou senha incorretos.");
+        } else {
+          return Alert.alert("Login", "Não foi possível realizar o login.");
+        }
+      })
+      .finally(() => setIsLogging(false));
+
+  }
+
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{
+      signIn,
+      isLogging,
+      user
+    }}>
       {children}
     </AuthContext.Provider>
   );
