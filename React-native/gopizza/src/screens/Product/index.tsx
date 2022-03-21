ec0a5cb2e5236174389bba59c930e4971319c4ca
David <davidwdiniz@gmail.com>
2022-03-21 09:42:59 -0300

    ImagePicker utilization

--- React-native/gopizza/src/screens/Product/index.tsx
+++ React-native/gopizza/src/screens/Product/index.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { Container, DeleteLabel, Header, PickImageButton, Title, Upload } from "./styles";
 import {Platform, TouchableOpacity} from "react-native";
 import {ButtonBack} from "../../components/ButtonBack";
 import {Photo} from "../../components/Photo";
+import * as ImagePicker from 'expo-image-picker';
 
 export function Product() {
+  const [image, setImage] = useState("");
+
+  async function handlePickImage() {
+    const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status === "granted") {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4]
+      });
+
+      if (result.cancelled === false) {
+        setImage(result.uri);
+      }
+    }
+  }
+
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <Header>
@@ -15,8 +32,12 @@ export function Product() {
         </TouchableOpacity>
       </Header>
       <Upload>
-        <Photo uri="" />
-        <PickImageButton title="Carregar" type="secondary" />
+        <Photo uri={image} />
+        <PickImageButton
+          title="Carregar"
+          type="secondary"
+          onPress={handlePickImage}
+        />
       </Upload>
     </Container>
   );
