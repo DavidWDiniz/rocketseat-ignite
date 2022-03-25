75f5bfbf94a3ba4dd8d2c9b3b98b8809a389581d
David <davidwdiniz@gmail.com>
2022-03-25 10:28:36 -0300

    ProductCard component

--- React-native/gopizza/src/screens/Home/index.tsx
+++ React-native/gopizza/src/screens/Home/index.tsx
@@ -5,6 +5,7 @@ import {MaterialIcons} from "@expo/vector-icons";
 import {useTheme} from "styled-components/native";
 import {TouchableOpacity} from "react-native";
 import {Search} from "../../components/Search";
+import {ProductCard} from "../../components/ProductCard";
 
 export function Home() {
   const theme = useTheme();
@@ -26,6 +27,13 @@ export function Home() {
         <Title>Cardápio</Title>
         <MenuItemsNumber>10 pizzas</MenuItemsNumber>
       </MenuHeader>
+
+      <ProductCard data={{
+        id: "1",
+        name: "Pizza",
+        description: "4 queijos",
+        photo_url: "https://superprix.vteximg.com.br/arquivos/ids/204058-600-600/8d1c807dd5f808c791d467a8f6391cf2_pizza-seara-de-4-queijos-460g_lett_5.jpg?v=637695827205500000"
+      }}/>
     </Container>
   );
 }
