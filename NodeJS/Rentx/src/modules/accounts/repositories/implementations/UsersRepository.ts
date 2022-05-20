d1f8717d0c55b64ac660487c7e90cf61d4392089
david <davidwdiniz@gmail.com>
2022-05-20 10:56:16 -0300

    Route authentication and error handling

--- NodeJS/Rentx/src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ NodeJS/Rentx/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -30,6 +30,10 @@ class UsersRepository implements IUsersRepository {
   async findByEmail(email: string): Promise<User> {
     return this.repository.findOneBy({ email });
   }
+
+  findById(id: string): Promise<User> {
+    return this.repository.findOneBy({ id });
+  }
 }
 
 export { UsersRepository };
