80155e4e38590ceb4ae7e9dabb498cc5431d0204
david <davidwdiniz@gmail.com>
2022-06-01 09:58:11 -0300

    Admin middleware

--- NodeJS/Rentx/src/shared/infra/http/routes/categories.routes.ts
+++ NodeJS/Rentx/src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from "multer";
 import { CreateCategoryController } from "../../../../modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "../../../../modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "../../../../modules/cars/useCases/listCategories/ListCategoriesController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const categoryRoutes = Router();
 
@@ -15,13 +17,20 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoryController = new ListCategoriesController();
 
-categoryRoutes.post("/", createCategoryController.handle);
+categoryRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
 categoryRoutes.get("/", listCategoryController.handle);
 
 categoryRoutes.post(
   "/import",
   upload.single("file"),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle
 );
 
