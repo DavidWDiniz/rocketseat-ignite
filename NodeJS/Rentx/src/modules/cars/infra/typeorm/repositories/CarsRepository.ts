6af70a057d30c3a7f5e2c94f5dcd018c943972d3
david <davidwdiniz@gmail.com>
2022-07-27 18:48:56 -0300

    Fix status car

--- NodeJS/Rentx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ NodeJS/Rentx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -71,6 +71,15 @@ class CarsRepository implements ICarsRepository {
   async findById(id: string): Promise<Car> {
     return this.repository.findOneBy({ id });
   }
+
+  async updateAvailable(id: string, available: boolean): Promise<void> {
+    await this.repository
+      .createQueryBuilder()
+      .update()
+      .set({ available })
+      .where("id = :id")
+      .setParameters({ id });
+  }
 }
 
 export { CarsRepository };
--- NodeJS/Rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ NodeJS/Rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 
 import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "../../../../shared/errors/AppError";
+import { ICarsRepository } from "../../../cars/repositories/ICarsRepository";
 import { IRentalsRepository } from "../../repositories/IRentalsRepository";
 
 interface IRequest {
@@ -16,7 +17,9 @@ class CreateRentalUseCase {
     @inject("RentalsRepository")
     private rentalsRepository: IRentalsRepository,
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
   ) {}
 
   async execute({ car_id, expected_return_date, user_id }: IRequest) {
@@ -48,11 +51,15 @@ class CreateRentalUseCase {
       throw new AppError("Invalid return time!");
     }
 
-    return this.rentalsRepository.create({
+    const rental = await this.rentalsRepository.create({
       user_id,
       car_id,
       expected_return_date,
     });
+
+    await this.carsRepository.updateAvailable(user_id, false);
+
+    return rental;
   }
 }
 
